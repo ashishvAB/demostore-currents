9a8591f8d567c188a8896467de64eb0cf4071fa5
ashishvAB <ashishkumar.alphabin@gmail.com>
2025-08-29 18:57:41 +0530

    new tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,2 +1,30 @@
-import config from "./pw.config.shared";
-export default config;
+// playwright.config.ts
+import { defineConfig, devices, PlaywrightTestConfig } from "@playwright/test";
+import { currentsReporter } from "@currents/playwright";
+
+export default defineConfig({
+  testDir: "./tests",
+  use: {
+    baseURL: "https://demo.alphabin.co",
+    trace: "on",
+    video: "on",
+    screenshot:"on",
+  },
+  fullyParallel: true,
+  timeout: 30000,
+  expect: {
+    timeout: 5000,
+  },
+  workers:1 ,
+  reporter: [currentsReporter()],
+  
+  projects: [
+    {
+      name: "Project A",
+      retries: 1,
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+  ],
+});
